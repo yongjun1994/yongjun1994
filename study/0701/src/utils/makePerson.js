063899a01b937cfb5c709b71eedc623606f5d25e
yongjun1994 <iyongjun089@gmail.com>
2025-07-01 16:56:11 +0900

    정리: 파일 추가 및 삭제 반영

---
+++ study/0701/src/utils/makePerson.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.makePerson = makePerson;
+exports.testMakePerson = testMakePerson;
+function makePerson(name, age) {
+    return { name, age };
+}
+function testMakePerson() {
+    console.log(makePerson("Jack", 22), makePerson("Jack", 33));
+}
---
+++ study/0701/src/utils/makePerson.ts
@@ -0,0 +1,9 @@
+export function makePerson(name: string, age: number) {
+  return {name, age};
+}
+
+export function testMakePerson() {
+  console.log(makePerson("Jack", 22),
+              makePerson("Jack", 33));
+}
+
