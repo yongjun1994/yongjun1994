cb1a34cbee347d64ee99c8c302e8185cd81ce557
Yong Jun Lee (Michael) <iyongjun089@gmail.com>
2025-07-03 10:20:54 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    
+    if (n % 7 === 0)
+        answer = Math.floor(n / 7);
+    else
+        answer = Math.floor(n / 7) + 1;
+    
+    return answer;
+}
