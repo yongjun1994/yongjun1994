a50f80ffbba88f3249718ace69aa5e78550b0b5c
Yong Jun Lee (Michael) <iyongjun089@gmail.com>
2025-07-07 10:21:30 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,14 @@
+function solution(sides) {
+    
+     let answer = 0;
+    
+     sides.sort((a, b) => b - a);
+    
+     if (sides[0] < sides[1] + sides[2]) {
+         answer = 1;
+     } else {
+         answer = 2;
+     }
+    
+     return answer 
+}
