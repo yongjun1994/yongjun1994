763fc21f3fc6aaddbaad404141a3afe0579bd2d0
Yong Jun Lee (Michael) <iyongjun089@gmail.com>
2025-07-10 10:42:55 +0900

    [level 2] Title: 올바른 괄호, Time: 27.43 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,15 @@
+function solution(s){
+    let count = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') 
+            count ++;
+        else
+            count --;
+        
+        if (count < 0)
+            return false;
+        
+    }
+    return count === 0;
+}
