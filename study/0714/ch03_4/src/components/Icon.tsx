b814e3234794bd0ab98bc48945cdb6aa7b540e54
yongjun1994 <iyongjun089@gmail.com>
2025-07-14 16:28:34 +0900

    2025년 7월 14일 18:28 실습 업로드

---
+++ study/0714/ch03_4/src/components/Icon.tsx
@@ -0,0 +1,15 @@
+import type { FC } from "react";
+
+export type IconProps = {
+  name: string;
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+export const Icon: FC<IconProps> = ({ name, style }) => {
+  return (
+    <span className="material-icons" style={style}>
+      {name}
+    </span>
+  );
+};
